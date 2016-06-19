3c91b941694f8327efd74efe20fb67a4d8332fa1
almostvindiesel <john@marsland.org>
2016-06-20 00:11:33 +0200

    updated for heroku

--- README.md
+++ README.md
@@ -3,3 +3,19 @@ virtualenv venv
 . venv/bin/activate
 pip install -r requirements.txt
 python runserver.py
+
+
+# For Heroku
+Run virtualenv venv
+Run source venv/bin/activate
+Run pip install flask (no sudo needed)
+Run pip install gunicorn
+Run pip install flask-sqlalchemy
+Run pip freeze > requirements.txt
+Add psycopg2 to the end of requirements.txt
+Run heroku addons:add shared-database
+Run git push heroku master
+heroku create
+git push heroku master
+heroku ps:scale web=1
+heroku open
--- the500days/views.py
+++ the500days/views.py
@@ -2,7 +2,6 @@ import requests
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 
 from the500days import app
-#from executionmeal import models
 
 @app.route('/500days')
 @app.route('/')
@@ -10,4 +9,10 @@ def show_entries():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    
+	# !!! uncomment for running locally
+    # app.run(debug=True)
+
+    # heroku
+    port = int(os.environ.get('PORT',5000))
+    app.run(host='0.0.0.0', port=port)
