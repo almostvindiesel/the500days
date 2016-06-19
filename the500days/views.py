92379777de321788f2c1bd440d457a13256287b9
almostvindiesel <john@marsland.org>
2016-06-20 00:34:40 +0200

    updated views to run on heroku

--- the500days/views.py
+++ the500days/views.py
@@ -15,4 +15,5 @@ if __name__ == '__main__':
 
     # heroku
     port = int(os.environ.get('PORT',5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    #app.run(host='0.0.0.0', port=port)
