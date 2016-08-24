4218200c3733362f4c8e9c772598428074305f13
almostvindiesel <john@marsland.org>
2016-08-24 22:24:07 +0200

    added pics for fb profiles; updated google location

--- the500days/views.py
+++ the500days/views.py
@@ -15,13 +15,12 @@ from the500days import app
 @app.route('/500days')
 @app.route('/')
 def show_entries():
+    print '-'*50
+
     num_days_into_trip = days_into_trip()
     location = get_location_from_last_foursquare_checkin()
     gmaps_url= get_static_google_map(location)
-    print '-'*50
-    print gmaps_url
-    print '-'*50
-
+    
     return render_template('index.html', num_days_into_trip=num_days_into_trip, location=location, gmaps_url=gmaps_url)
 
 def get_static_google_map(location):
@@ -55,9 +54,10 @@ def get_location_from_last_foursquare_checkin():
     try:
         #raise Exception('spam', 'eggs')
         foursquare_url = 'https://feeds.foursquare.com/history/6e67d1fa352b2bd8480c98adcf91d2a4.rss'
+        print "Gettng data from foursquare api: \r\n", foursquare_url
+
         r = requests.get(foursquare_url)
         check_in_xml = r.text
-
         check_in_ordered_dict = xmltodict.parse(check_in_xml)
         check_in_json = json.dumps(check_in_ordered_dict['rss']['channel']['item'][0], indent=4)
 
@@ -67,17 +67,38 @@ def get_location_from_last_foursquare_checkin():
         latitude = lat_long_list[0]
         longitude = lat_long_list[1]
 
-        #Get City from Google Maps API
-        googlemaps_url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + str(latitude) + 	"," + str(longitude) + "&sensor=true"
-        rg = requests.get(googlemaps_url)
-        geolocation_json = rg.json()
-        city = geolocation_json['results'][0]['address_components'][3]['short_name']
-        country = geolocation_json['results'][0]['address_components'][6]['long_name']
+        print "--- From FS API latitude: ", latitude
+        print "--- From FS API longitude: ", longitude
+        print ""
 
-        # !!! add venue to 500 days, too!
-        location  = {'city': city, 'country': country, 'latitude': latitude, 'longitude': longitude, 'venue': venue}
-    except:
-        print("Problem getting location from foursquare api")
-        location  = None
+        #Get City from Google Maps API
+        googlemaps_url = "http://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&sensor=false" % (latitude, longitude)
+        print "Gettng location data from google maps api : \r\n", googlemaps_url
+
+        try: 
+            r = requests.get(googlemaps_url)
+            g_json = r.json()
+            for datums in g_json['results'][0]['address_components']:
+                if datums['types'][0] == 'locality':
+                    city = datums['long_name']
+                    print "--- From Google Lat Long API, City: ", city
+                if datums['types'][0] == 'country':
+                    country = datums['long_name']
+                    print "--- From Google Lat Long API, Country: ", country
+
+            location = {'city': city, 'country': country, 'latitude': latitude, 'longitude': longitude, 'venue': venue}
+
+        except Exception as e:
+            print "Could not get data from google api: ", e.message, e.args
+            city = None
+            country = None
+
+    except Exception as e:
+        print "Could not get data from foursquare api api: ", e.message, e.args
+        city = None
+        country = None
+
+    #location = {'city': None, 'country': None, 'latitude': latitude, 'longitude': longitude, 'venue': venue}
+
 
     return location
